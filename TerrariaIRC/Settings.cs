f399f7003b6c8c7aa07259ab1b5f246a9bff1ace
Deathmax <death@deathmax.tk>
2012-01-14 11:13:53 +0800

    TerrariaIRC : More progress, connects to a server.

---
+++ TerrariaIRC/Settings.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.IO;
+
+namespace TerrariaIRC
+{
+    internal class Settings : Dictionary<string, string>
+    {
+        public bool Load()
+        {
+            string[] file;
+            if (File.Exists(TerrariaIRC.settingsfiles))
+                file = File.ReadAllLines(TerrariaIRC.settingsfiles);
+            else
+                return false;
+            foreach (var t in file.Where(t => !t.StartsWith("//") && !t.StartsWith("#")).Where(t => t.Split('=').Length == 2))
+                Add(t.Split('=')[0].ToLower(), t.Split('=')[1]);
+            return CheckKeys();
+        }
+
+        private bool CheckKeys()
+        {
+            var server = false;
+            var port = false;
+            var allowop = false;
+            var channel = false;
+            var name = false;
+            foreach (var pair in this)
+            {
+                switch (pair.Key)
+                {
+                    case "server":
+                        server = true;
+                        break;
+                    case "port":
+                        int s;
+                        if (int.TryParse(pair.Value, out s))
+                            port = true;
+                        break;
+                    case "allowop":
+                        if (bool.TryParse(pair.Value, out allowop))
+                            allowop = true;
+                        break;
+                    case "channel":
+                        channel = true;
+                        break;
+                    case "botname":
+                        name = true;
+                        break;
+                }
+            }
+            return (server && port && allowop && channel && name);
+        }
+    }
+}
--- TerrariaIRC/TerrariaIRC.cs
+++ TerrariaIRC/TerrariaIRC.cs
@@ -1,11 +1,14 @@
 ﻿using System;
+using System.IO;
 using Hooks;
 using Meebey.SmartIrc4net;
 using TShockAPI;
 using Terraria;
+using ErrorEventArgs = Meebey.SmartIrc4net.ErrorEventArgs;
 
 namespace TerrariaIRC
 {
+    [APIVersion(1, 10)]
     public class TerrariaIRC : TerrariaPlugin
     {
         #region Plugin Properties
@@ -34,6 +37,8 @@ namespace TerrariaIRC
 
         #region Plugin Vars
         public static IrcClient irc = new IrcClient();
+        private static Settings settings = new Settings();
+        public static string settingsfiles = Path.Combine(TShock.SavePath, "irc", "settings.txt");
         #endregion
 
         #region Plugin overrides
@@ -42,8 +47,17 @@ namespace TerrariaIRC
             irc.Encoding = System.Text.Encoding.ASCII;
             irc.SendDelay = 300;
             irc.ActiveChannelSyncing = true;
-
+            irc.OnQueryMessage += OnQueryMessage;
+            irc.OnError += OnError;
+            irc.OnRawMessage += OnRawMessage;
+            if (!settings.Load())
+            {
+                Console.WriteLine("Settings failed to load, aborting IRC.");
+                return;
+            }
+            Connect();
         }
+
         protected override void Dispose(bool disposing)
         {
             if (disposing)
@@ -51,5 +65,44 @@ namespace TerrariaIRC
             }
         }
         #endregion
+
+        public static void OnQueryMessage(object sender, IrcEventArgs e)
+        {
+            Console.WriteLine("Query: {0}", e.Data.RawMessage);
+        }
+
+        public static void OnError(object sender, ErrorEventArgs e)
+        {
+            Console.WriteLine("Error: {0}", e.Data.RawMessage);
+        }
+
+        public static void OnRawMessage(object sender, IrcEventArgs e)
+        {
+            Console.WriteLine("Raw: {0}", e.Data.RawMessage);
+        }
+
+        public void Connect()
+        {
+            while (true)
+            {
+                Console.WriteLine("Connecting to {0}:{1}...", settings["server"], settings["port"]);
+                try
+                {
+                    irc.Connect(settings["server"], int.Parse(settings["port"]));
+                    Console.WriteLine("Connected to IRC server.");
+                }
+                catch (Exception e)
+                {
+                    Console.WriteLine("Error connecting to IRC server.");
+                    Console.WriteLine(e);
+                    return;
+                }
+                irc.Login(settings["botname"], "TerrariaIRC");
+                irc.RfcJoin(settings["channel"]);
+                Console.WriteLine("Joined {0}", settings["channel"]);
+                irc.Listen();
+                Console.WriteLine("Disconnected from IRC... Attempting to reconnect");
+            }
+        }
     }
 }
